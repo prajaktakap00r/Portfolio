02c487500dd4c773d0a2acce19496dba308fc455
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-05-25 15:46:31 +0530

    projects updated

--- src/ContentBody/Projects.jsx
+++ src/ContentBody/Projects.jsx
@@ -3,14 +3,54 @@
 import * as React from "react";
 import { CardContent, useMediaQuery } from "@mui/material";
 import Card from "@mui/material/Card";
-import SN from "../images/SN.png";
-import todo from "../images/Todo.png";
-function CoreConcept({ image, title, description }) {
+import SN from "../images/social.svg";
+import TODO from "../images/todo.svg";
+import Heart from "../images/heart.svg";
+import calculator from "../images/calculator.svg";
+import wedding from "../images/gurepe.svg";
+import email from "../images/email.svg";
+import restaurant from "../images/restaurant.svg";
+import portfolio from "../images/portfolio.svg";
+const itemData = [
+  {
+    img: SN,
+    title: "Social Network",
+  },
+  {
+    img: TODO,
+    title: "WhatToDo",
+  },
+  {
+    img: Heart,
+    title: "Heart Disease Prediction Model",
+  },
+  {
+    img: wedding,
+    title: "GuRePe",
+  },
+  {
+    img: email,
+    title: "Email Template",
+  },
+  {
+    img: restaurant,
+    title: "Restaurant App",
+  },
+  {
+    img: calculator,
+    title: "SGPA Calculator",
+  },
+  {
+    img: portfolio,
+    title: "Portfolio",
+  },
+];
+
+function CoreConcept({ img, title }) {
   return (
     <li>
-      <img src={image} alt={title} />
+      <img src={img} alt={title} />
       <h3>{title}</h3>
-      <p>{description}</p>
     </li>
   );
 }
@@ -31,10 +71,7 @@ export default function Projects() {
             <section id="core-concepts">
               <h2>Projects</h2>
               <ul>
-                <CoreConcept
-                  title={itemData[0].title}
-                  image={itemData[0].img}
-                />
+                <CoreConcept title={itemData[0].title} img={itemData[0].img} />
                 <CoreConcept {...itemData[1]} />
                 <CoreConcept {...itemData[2]} />
                 <CoreConcept {...itemData[3]} />
@@ -50,47 +87,3 @@ export default function Projects() {
     </Card>
   );
 }
-
-// Your itemData array remains unchanged...
-
-const itemData = [
-  {
-    img: SN,
-    title: "Social Network",
-  },
-  {
-    img: todo,
-    title: "WhatToDo",
-    author: "@rollelflex_graphy726",
-  },
-  {
-    img: "https://static.vecteezy.com/system/resources/previews/004/462/252/original/modern-heart-medication-heart-disease-research-concept-cardiologist-studying-big-heart-model-drugs-and-heartbeat-diagram-illustration-cardiovascular-system-cardiology-medical-examination-vector.jpg",
-    title: "Heart Disease Prediction Model",
-    author: "@helloimnik",
-  },
-  {
-    img: "https://png.pngtree.com/png-clipart/20190117/ourlarge/pngtree-plant-flower-branch-purple-flowers-illustration-png-image_439846.jpg",
-    title: "GuRePe",
-    author: "@nolanissac",
-  },
-  {
-    img: "https://sendgrid.com/content/dam/sendgrid/legacy/2021/09/Untitled-design-53.png",
-    title: "Email Template",
-    author: "@hjrc33",
-  },
-  {
-    img: "https://thumbs.dreamstime.com/b/restaurant-building-flat-design-architecture-facade-food-cafe-construction-vector-illustration-69417405.jpg",
-    title: "Restaurant App",
-    author: "@arwinneil",
-  },
-  {
-    img: "https://p7.hiclipart.com/preview/833/721/133/calculator-clip-art-calculator.jpg",
-    title: "SGPA Calculator",
-    author: "@tjdragotta",
-  },
-  {
-    img: "https://www.pngitem.com/pimgs/m/17-170452_taxi-hire-website-design-web-design-illustration-png.png",
-    title: "Portfolio",
-    author: "@katie_wasserman",
-  },
-];
